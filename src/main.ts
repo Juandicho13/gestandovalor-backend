52e5ca45d57a635e533088e79b96da09339c4dfa
Juan David Olarte Diaz <juanolartediaz13@gmail.com>
2026-03-04 23:24:30 -0500

    Aumento limite de peso para fotos base64

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // ¡Agregamos esta línea mágica para darle permiso a tu HTML!
-  app.enableCors(); 
 
-  await app.listen(process.env.PORT ?? 3000);
+  // 1. Asegurar que las webs externas puedan conectarse (CORS)
+  app.enableCors();
+
+  // 2. LA MAGIA: Aumentar el límite de peso a 50 Megabytes para que pasen las fotos
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
