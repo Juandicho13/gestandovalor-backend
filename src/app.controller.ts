d94d2b1faf58a942ea9dfd775e44dcf93b43c265
Juan David Olarte Diaz <juanolartediaz13@gmail.com>
2026-03-17 12:39:57 -0500

    test: prueba de vida con ruta ping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,16 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // ✨ LA PRUEBA DEFINITIVA DE VIDA ✨
+  @Get('ping')
+  ping() {
+    return { mensaje: '¡EL SERVIDOR SÍ SE ESTÁ ACTUALIZANDO!', status: 'OK' };
   }
 }
