7d088f07aa8c8df115af97f9ad4e6e1dd51ee586
Juan David Olarte Diaz <juanolartediaz13@gmail.com>
2026-03-26 22:21:45 -0500

    feat: habilitar ruta DELETE para poder borrar prospectos/solicitudes

--- src/prospectos/prospectos.controller.ts
+++ src/prospectos/prospectos.controller.ts
@@ -1,20 +1,35 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ProspectosService } from './prospectos.service';
 import { CreateProspectoDto } from './dto/create-prospecto.dto';
+import { UpdateProspectoDto } from './dto/update-prospecto.dto';
 
 @Controller('prospectos')
 export class ProspectosController {
-  constructor(private readonly prospectosService: ProspectosService) {}
+  constructor(private readonly prospectosService: ProspectosService) { }
 
-  // Este es el punto de entrada para el Formulario Web
   @Post()
   create(@Body() createProspectoDto: CreateProspectoDto) {
     return this.prospectosService.create(createProspectoDto);
   }
 
-  // Este te servirá a ti para ver la lista de interesados
   @Get()
   findAll() {
     return this.prospectosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.prospectosService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProspectoDto: UpdateProspectoDto) {
+    return this.prospectosService.update(id, updateProspectoDto);
+  }
+
+  // ✨ ESTO ES LO QUE NOS FALTABA ✨
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.prospectosService.remove(id);
   }
 }
