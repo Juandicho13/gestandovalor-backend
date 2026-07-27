b616974273c82ecc17168666a0a6c6adeb869a8f
Juan David Olarte Diaz <juanolartediaz13@gmail.com>
2026-07-26 20:14:15 -0500

    perf: activar compresion gzip en las respuestas

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 // ✨ IMPORTA ESTO ARRIBA ✨
 import { json, urlencoded } from 'express';
-import * as compression from 'compression';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +11,6 @@ async function bootstrap() {
   app.enableCors();
   app.use(compression());
 
-  // ✨ AQUÍ ESTÁ LA MAGIA: AMPLIAMOS EL LÍMITE A 50MB ✨
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
